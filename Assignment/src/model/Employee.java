939b25fa3be4dc7a4fcd88a10c8643ec2d82cbae
VatsalDoshi <doshi.va@northeastern.edu>
2022-10-02 01:51:44 -0400

    fourth commit

---
+++ Assignment/src/model/Employee.java
@@ -0,0 +1,91 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author doshi
+ */
+public class Employee {
+   
+    private String Create;
+    private String Read;
+    private String Update;
+    private String Delete;
+
+    public String getCreate() {
+        return Create;
+    }
+
+    public void setCreate(String Create) {
+        this.Create = Create;
+    }
+
+    public String getRead() {
+        return Read;
+    }
+
+    public void setRead(String Read) {
+        this.Read = Read;
+    }
+
+    public String getUpdate() {
+        return Update;
+    }
+
+    public void setUpdate(String Update) {
+        this.Update = Update;
+    }
+
+    public String getDelete() {
+        return Delete;
+    }
+
+    public void setDelete(String Delete) {
+        this.Delete = Delete;
+    }
+
+    public void setName(String Name) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setEmployeeId(String EmployeeId) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setAge(String Age) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setGender(String Gender) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setStartDate(String StartDate) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setLevel(String Level) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void PositionTitle(String PositionTitle) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setContactInfo(String ContactInfo) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setPhoto(String Photo) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setTeamInfo(String TeamInfo) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+}
---
+++ Assignment/src/model/EmployeeInfo.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author doshi
+ */
+public class EmployeeInfo {
+    private ArrayList<Employee> history;
+   
+    public EmployeeInfo() {
+        this.history = new ArrayList<Employee>();
+        
+        
+    }
+
+    public ArrayList<Employee> getHistory() {
+        return history;
+    }
+
+    public void setHistory(ArrayList<Employee> history) {
+        this.history = history;
+    }
+    
+    public Employee addNewEmployee(){
+        
+        Employee newEmployee = new Employee();
+        history.add(newEmployee);
+        return newEmployee;
+    
+}
+}
